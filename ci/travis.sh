640c10bcde7b246e1b5a0346a6b43c4c8bc8125c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2013-12-09 12:04:35 +0100

    FIX ansible script

--- ci/travis.sh
+++ ci/travis.sh
@@ -4,9 +4,8 @@
 echo "-------------------------------------"
 echo "----- Openstack VM with Ansible -----"
 echo "-------------------------------------"
-cd ansible
 echo "Check valid hosts"
-ansible -c local -m ping all -i ../ansible/ansible_hosts
+ansible -c local -m ping all -i ../ansible/dev_hosts
 
 echo "Configure"
-sudo ansible-playbook -c local -i ../ansible/ansible_hosts ../ansible/setup.yml --extra-vars="user=travis"
+sudo ansible-playbook -c local -i ../ansible/dev_hosts ../ansible/openstack_dev.yml --extra-vars="user=travis"
